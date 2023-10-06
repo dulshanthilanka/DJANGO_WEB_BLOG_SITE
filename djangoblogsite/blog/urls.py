e4a29faf7cc24e3d8afa23467a0016b7cddee5cd
dulshanthilanka <88085689+dulshanthilanka@users.noreply.github.com>
2023-10-06 23:49:16 +0530

    Update urls.pyupdate,detail,create,list adition

--- djangoblogsite/blog/urls.py
+++ djangoblogsite/blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 from . import views
 
-urlpatterns=[
- path('home/', views.home, name="home_path"),
- path('about/', views.about, name="about_path"),
- path('', views.home, name="home_path2"),
+urlpatterns = [
+    path('home/', PostListView.as_view(), name="home_path"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name="post-Update"),
+    path('about/', views.about, name="about_path"),
+    path('', views.home, name="home_path2"),
 ]
